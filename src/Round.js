927318d5933d355f6f166d51ef716356fc5c8475
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-03-22 20:13:22 -0600

    feat:Add four tests to the Round class tests

--- src/Round.js
+++ src/Round.js
@@ -2,7 +2,18 @@ class Round {
   constructor(deck) {
   this.deck = deck;
   this.currentCard = this.deck.cards[0];
+  this.turns = 0;
+  this.incorrect = [];
   }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    this.turns ++
+  }
+
 };
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -3,6 +3,7 @@ const assert = chai.assert;
 
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe("Round", function() {
@@ -15,6 +16,7 @@ describe("Round", function() {
 
   deck = new Deck([card1, card2, card3]);
   round = new Round(deck);
+  guess = new Turn();
   });
 
   it("should have a property of deck", function() {
@@ -23,8 +25,33 @@ describe("Round", function() {
 
   it("should have a currentCard property which should be the first Card in the Deck (the array of Cards) at the start of the Round", function() {
     assert.deepEqual(round.currentCard, round.deck.cards[0]);
-
   });
-    
+
+  it("should return the current card being played", function() {
+    assert.deepEqual(round.returnCurrentCard(), round.currentCard);
+  })
    
+  });
+
+  describe("takeTurn method in Round Class", function() {
+    let card1, card2, card3, deck, round;
+
+  beforeEach(function() {
+  card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+  deck = new Deck([card1, card2, card3]);
+  round = new Round(deck);
+  guess = new Turn();
+  });
+
+    it("should update turns count when a guess is made", function() {
+      assert.equal(round.turns, 1)
+    });
+
+    it("should store ids of incorrect guesses", function() {
+      
+    })
+
   });
